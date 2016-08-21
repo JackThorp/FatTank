244159f2b8945df8e8f52b5d214f878815421a27
Jack Thorp <jackthorp13@gmail.com>
2016-08-21 12:15:36 +0100

    Almost finished with navigation bar. Niggling issues remaining including breakpoint for collapse, padding of social media icons and padding of brand and collapse button on small screens

--- site/web/app/themes/alicia/templates/header.php
+++ site/web/app/themes/alicia/templates/header.php
@@ -3,22 +3,19 @@
     <div class="row">
       <img id="landing-img" src="<?= get_template_directory_uri(); ?>/dist/images/frontpic.jpg">
       <nav id="navbar" class="nav-down">
-        <div id="navcontainer">
           <ul class="topnav" id="Topnav">
-            <li><img src="<?= get_template_directory_uri(); ?>/dist/images/Fattank-logovit.png"></li>
-            <li><a href="#section-start">Start</a></li>
+            <li><a href="#section-start"><img src="<?= get_template_directory_uri(); ?>/dist/images/Fattank-logovit.png"></a></li>
+            <!--<li><a href="#section-start">Start</a></li>-->
             <li><a href="#section-rehearse">Rehearse</a></li>
             <li><a href="#section-record">Record</a></li>
             <li><a href="#section-residents">Residents</a></li>
             <li><a href="#section-radio">Radio</a></li>
             <li><a href="#section-about">About</a></li>
             <li><a href="#section-contact">Contact</a></li>
+            <li class="social-icon"><a><i class="fa fa-2x fa-facebook"></i></a></li>
+            <li class="social-icon"><a><i class="fa fa-2x fa-twitter"></i></a></li>
             <li class="icon"><a href="javascript:void(0);" onclick="menuToggle()">&#9776;</a></li>  
-            <!--<li><a><i class="fa fa-facebook"></i></a></li>
-            <li><a><i class="fa fa-twitter"></i></a></li>
-            -->
           </ul>
-        </div>
       </nav>
     </div>
   </div>
